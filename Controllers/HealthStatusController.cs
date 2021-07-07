41ed0f045828b1f61ff9335ba3be8c42fb95229b
Maclain Wiltzer <mwiltzer@gmail.com>
2021-07-07 11:06:01 -0400

    Add service stub files

---
+++ Controllers/HealthStatusController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+
+namespace ServiceStub.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthStatusController : ControllerBase
+    {
+        public static HealthStatus Status { get; set; } = HealthStatus.Healthy;
+        [HttpGet()]
+        public string Get()
+        {
+            Console.WriteLine("Request received: GET /HealthStatus");
+            return Status.ToString();
+        }
+    }
+}
